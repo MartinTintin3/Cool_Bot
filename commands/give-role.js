dc3252d61191ebf6e14912f4241b1c6f9f0ad356
martinGITHUBER <maroymart@gmail.com>
2020-08-06 18:06:04 -0400

    Update give-role.js

--- commands/give-role.js
+++ commands/give-role.js
@@ -1,5 +1,29 @@
 module.exports = {
+	name: 'give-role',
+	aliases: ['add-role'],
+	description: 'Gives the specified role to the specified user in the specified server/guild',
+	usage: '<role name(case sensitive)> <user id> <server/guild id>',
+	args: true,
+	args_num: 3,
+	category: 'Utility',
+	private: true,
 	execute(message, args){
+		const guildId = args[2];
+		const memberId = args[1];
+		const roleName = args[0];
+		const role = message.client.guilds.cache.get(guildId).roles.cache.find(r => r.name == roleName);
 
+		if(!message.client.guilds.cache.get(guildId).me.hasPermission('MANAGE_ROLES')){
+			return message.channel.send('I am sorry but I don\'t have the neccesary permissions on the server you specified to give the user you specified a role');
+		}
+
+		message.client.guilds.cache.get(guildId).members.cache.get(memberId).roles.add(role)
+			.then(r =>{
+				message.channel.send(`Gave you the ${r.name} role!`);
+			})
+			.catch(err => {
+				console.error(err);
+				return message.channel.send(err);
+			});
 	},
 };
