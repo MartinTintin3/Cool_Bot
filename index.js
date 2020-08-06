5aec87542ad48cf1a453eb7634cc02f63f2a824d
martinGITHUBER <maroymart@gmail.com>
2020-08-06 17:46:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -187,8 +187,9 @@ client.on('message', message => {
 			}else if(args[0] == 'help') {
 				return client.commands.get('help').execute(message, [command.name]);
 			}
-
-			command.execute(message, args);
+			if(!command.private || (command.private && message.channel.type == 'dm')){
+				command.execute(message, args);
+			}
 		}catch (error) {
 			console.error(error);
 			message.reply('there was an error trying to execute that command!');
