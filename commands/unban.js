8cb1496d44962b82658565c60152e271901fca55
martinGITHUBER <maroymart@gmail.com>
2020-08-06 17:44:22 -0400

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -6,4 +6,9 @@ module.exports = {
 	args_num: 2,
 	category: 'Utility',
 	private: true,
+	execute(message, args){
+		message.client.guilds.cache.get(args[1]).members.unban(args[0])
+			.then(user => console.log(`Unbanned ${user.username} from ${message.client.guilds.cache.get(args[1]).name}`))
+			.catch(console.error);
+	},
 };
