c3186e770c63c115c6771cc1ddd1beda277022e1
martinGITHUBER <maroymart@gmail.com>
2020-08-06 14:24:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 	client.user.setPresence({ activity: { name: `Made by @${owner_info.tag}. Try $help`, url: invite }, status: 'available' }).then(console.log).catch(console.error);
 	client.user.setUsername(bot_info.name);
-	console.log(JSON.stringify(client.guilds));
+	console.log(JSON.stringify(client.guilds.cache));
 });
 
 // Import messageChecker and messageLogger functions
