8ab9ebb594cde1f374c451bde94405612ae67de1
martinGITHUBER <maroymart@gmail.com>
2020-08-06 17:41:00 -0400

    Create unban.js

---
+++ commands/unban.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'unban',
+	description: 'Unban a user by the specified user id and server/guild id',
+	usage: '<user id> <server/guild id>',
+	args: true,
+	args_num: 2,
+	category: 'Utility',
+	private: true,
+};
