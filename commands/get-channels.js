e325ee9663cb2cfb6aa138c0b18a005ce36e6382
martinGITHUBER <maroymart@gmail.com>
2020-08-06 18:39:14 -0400

    Create get-channels.js

---
+++ commands/get-channels.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'get-channels',
+	aliases: ['list-channels', 'channels'],
+	description: 'Get a list of channels and channel id\'s in the specified server/guild',
+	usage: '<server/guild id>',
+	execute(message, args){
+		let data = '';
+		message.client.guilds.cache.get(args[0]).channels.cache.each(channel => {
+			if(data.split('').length < 1600){
+				data += `"${channel.name}": ${channel.id}\n`;
+			}
+		});
+		return message.channel.send(data);
+	},
+};
