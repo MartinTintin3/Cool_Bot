cf65e606b5e370d9cccdbdb119c8a117e40bea2c
martinGITHUBER <maroymart@gmail.com>
2020-08-07 13:51:51 -0400

    Update audit-log.js

--- commands/audit-log.js
+++ commands/audit-log.js
@@ -3,10 +3,9 @@ module.exports = {
 	aliases: ['audit-logs', 'audit'],
 	description: 'Fetch the audit logs for the specified server/guild',
 	category: 'Info',
-	usage: '<server/guild id> <number of entries>',
+	usage: '<server/guild id> <number of entries> <"force" is optional and if provided will make multiple messages if one message is longer than 2000 characters>',
 	private: true,
 	args: true,
-	args_num: 2,
 	execute(message, args){
 		message.client.guilds.cache.get(args[0]).fetchAuditLogs()
 			.then(audit => {
@@ -15,11 +14,18 @@ module.exports = {
 				for(const i in logs){
 					data.push(`Action: ${logs[i].action}\nTimestamp: ${logs[i].createdAt}\nExecuter: ${logs[i].executor.tag}\nTarget: ${logs[i].target}\nExtra: ${logs[i].extra}\n`);
 					for(const n in logs[i].changes){
-						data[data.length - 1] += `Changes: {\n\tKey: ${logs[i].changes[n].key}\nOld: ${JSON.stringify(logs[i].changes[n].old)}\nNew: ${JSON.stringify(logs[i].changes[n].new)}}`;
+						data[data.length - 1] += `Changes: {\n\tKey: ${logs[i].changes[n].key}\n\tOld: ${JSON.stringify(logs[i].changes[n].old)}\n\tNew: ${JSON.stringify(logs[i].changes[n].new)}}`;
 					}
 					data[data.length - 1] += '\n';
 				}
-				return message.channel.send(data.reverse().join('\n'));
+
+				const newData = data.reverse().join('\n');
+				if(newData > 2000 && args[2] != 'force'){
+					return message.channel.send('This message is to long. Please specify a smaller amount of entries');
+				}else if(args[2] == 'force'){
+					return message.channel.send(newData, { split: true });
+				}
+				return message.channel.send(newData);
 			})
 			.catch(console.error);
 	},
