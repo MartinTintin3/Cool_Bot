3e68b4cd729bc95ef232ed4697f22f8e9acb275a
martinGITHUBER <maroymart@gmail.com>
2020-08-07 08:26:55 -0400

    Update invite-bot.js

--- commands/invite-bot.js
+++ commands/invite-bot.js
@@ -2,7 +2,7 @@ const { invite } = require('../config.json');
 
 module.exports = {
 	name: 'invite-bot',
-	aliases: ['invitebot', 'invite_bot', 'inv-bot'],
+	aliases: ['invitebot', 'inv-bot', 'invite'],
 	description: 'If you are a server admin and have the power to invite bots, then use this command to invite me! I will need to be able to administrate so I can delete bad words and other things!',
 	category: 'Utility',
 	execute(message, args) {
