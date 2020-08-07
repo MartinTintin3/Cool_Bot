1669999dfe0ca87f6661560b009e848f06c22235
martinGITHUBER <maroymart@gmail.com>
2020-08-07 18:11:23 -0400

    Update invite-bot.js

--- commands/invite-bot.js
+++ commands/invite-bot.js
@@ -6,6 +6,6 @@ module.exports = {
 	description: 'If you are a server admin and have the power to invite bots, then use this command to invite me! I will need to be able to administrate so I can delete bad words and other things!',
 	category: 'Utility',
 	execute(message, args) {
-		message.channel.send(`**Here is the invite link:**\n${invite}`);
+		message.channel.send(`**Here is the invite link:**\n${invite_link}`);
 	},
 };
