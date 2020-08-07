a43ac8f9c77b3c70d54fe4ce9b075e86cabe7749
martinGITHUBER <maroymart@gmail.com>
2020-08-07 13:19:06 -0400

    Update audit-log.js

--- commands/audit-log.js
+++ commands/audit-log.js
@@ -13,7 +13,7 @@ module.exports = {
 				const logs = audit.entries.first(parseInt(args[1]));
 				let data = '';
 				for(const i in logs){
-					data += `Action: ${logs[i].action}\nExecuter: ${logs[i].executor.tag}\n`;
+					data += `Action: ${logs[i].action}\nExecuter: ${logs[i].executor.tag}\nTarget: ${logs[i].target}\n\n`;
 				}
 				return message.channel.send(data);
 			})
