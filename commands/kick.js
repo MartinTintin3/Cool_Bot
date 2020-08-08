1f56fd738c0723570ce9fba7e8d0ece1512a44b4
martinGITHUBER <maroymart@gmail.com>
2020-08-08 17:33:26 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'kick',
-	description: 'Kick the user by the mention or by their id. **Make sure you don\'t mention anyone else in the message or the person who also got mentioned might get banned**',
-	usage: '<@user or user id> <reason for kicking(Optional)>',
+	description: 'Kick the user by the mention. **Make sure you don\'t mention anyone else in the message or the person who also got mentioned might get kicked!**',
+	usage: '<@user> <reason for kicking(Optional)>',
 	category: 'Moderation',
 	args: true,
 	guildOnly: true,
@@ -9,22 +9,21 @@ module.exports = {
 		if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You need the Ban Members permission');
 		if(!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send(`I need the Ban Members permission. Please contact "${message.guild.owner.user.tag}" and tell them to give me the Ban Members permission`);
 
-		if (!message.mentions && args[0].length == 18) return message.channel.send('Please mention the user you want to kick or provide the id of the user');
+		if (!message.mentions && args[0].length == 18) return message.channel.send('Please mention the user you want to kick');
 
-		const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+		const member = message.mentions.members.first();
+		if(!member)
+		{return message.reply('Please mention a valid member of this server');}
+		if(!member.kickable)
+		{return message.reply('I cannot kick this user! Do they have a higher role?');}
 
-		console.log(user);
+		let reason = args.slice(1).join(' ');
+		if(!reason) reason = 'No reason provided';
 
-		if(args[1]){
-			const id = args.shift();
-			user.kick(args.join(' ')).then(member => {
-				message.guild.owner.send(`${member.tag} Has been kicked from ${message.guild.name} by ${message.author.tag} for \`${args.join(' ')}\``);
-				return message.channel.send(`Successfuly kicked ${member.tag} by ${message.author.tag} for \`${args.join(' ')}\``);
-			});
-		}
-		user.kick().then(member => {
-			message.guild.owner.send(`${member.tag} Has been kicked from ${message.guild.name} by ${message.author.tag} No reason spcefified`);
-			return message.channel.send(`Successfuly kicked ${member.tag} by ${message.author.tag} for \`${args.join(' ')}\``);
+		member.kick().then(m => {
+			console.log(JSON.stringify(member));
+			m.guild.owner.send(`\`${member.displayName}\` has been kicked from \`${message.guild.name}\` by \`${message.author.tag}\` for \`${reason}\``);
+			return message.channel.send(`Successfuly kicked ${member.displayName} by ${message.author.tag} for \`${reason}\``);
 		});
 	},
 };
