0c7720ce1865ae0bf9e2755e139ed1261fd72e44
martinGITHUBER <maroymart@gmail.com>
2020-08-25 07:44:33 -0400

    Update messageLogger.js

--- messageLogger.js
+++ messageLogger.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
 
 function logMessage(message){
+	const date = new Date();
+	let msg = message.cleanContent;
+
+	if(message.channel.type == 'dm'){
+		if (!fs.existsSync('../messageLogs/dms')){
+			fs.mkdirSync('../messageLogs/dms');
+		}
+		if(message.author.id == '731206123327193170') return;
+		return fs.appendFileSync(`../messageLogs/dms/${message.author.tag}.log`, `\n\n${date}: ${msg}`);
+	}
+
 	if(!message.guild) return;
 
-	let msg = message.cleanContent;
 	if(message.embeds.length){
 		msg = '[Discord Embed]';
 	}
 
-	if (!fs.existsSync(`../messageLogs/${message.guild.name}`)){
-		fs.mkdirSync(`../messageLogs/${message.guild.name}`);
+	if (!fs.existsSync(`../messageLogs/guilds/${message.guild.name}`)){
+		fs.mkdirSync(`../messageLogs/guilds/${message.guild.name}`);
 	}
 
-
-	const date = new Date();
-
-	fs.appendFileSync(`../messageLogs/${message.guild.name}/${message.channel.name}.log`, `\n\n${date} by ${message.author.tag}: ${msg}`);
+	fs.appendFileSync(`../messageLogs/guilds/${message.guild.name}/${message.channel.name}.log`, `\n\n${date} by ${message.author.tag}: ${msg}`);
 }
 
 module.exports = { logMessage };
